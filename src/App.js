db01ade6b54c04ec6096528fa4c67c0bd6550d19
Naveen Bhusare <79650422+Naveen-7000@users.noreply.github.com>
2023-08-10 09:43:56 +0530

    Completed the api intergration some styling is left to be done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to "REACT WORLD"</h1>
+      <h1>Student Managemnt</h1>
     </div>
   );
 }
